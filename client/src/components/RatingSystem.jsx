9f56c7224603ede7cb201a567cd262eeb35ebf85
kovkat <105742967+kovkat@users.noreply.github.com>
2025-06-15 23:27:17 +0300

    Update RatingSystem.jsx

--- client/src/components/RatingSystem.jsx
+++ client/src/components/RatingSystem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function RatingSystem({ rating }) {
-  // Only render the stars themselves:
   return (
     <div>
       {'⭐'.repeat(Math.round(rating))}
